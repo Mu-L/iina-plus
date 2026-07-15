9a5a813705383d67eba2f26e6e5e0e7e0fa52725
xjbeta <xjbeta.tiny@gmail.com>
2026-07-15 23:03:45 +0800

    misc: plugin tag

--- InstallDeps.sh
+++ InstallDeps.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
-# Download iina-plugin-danmaku
-wget -O 'IINA+/iina-plugin-danmaku.iinaplgz' $(curl -s https://api.github.com/repos/xjbeta/iina-plugin-danmaku/releases/latest | grep -wo "https.*.iinaplgz")
+REPO=xjbeta/iina-plugin-danmaku
 
+if [ -n "${1:-}" ]; then
+  gh release download "$1" --repo "$REPO" --pattern 'iina-plugin-danmaku.iinaplgz' --dir IINA+ --clobber
+else
+  gh release download --repo "$REPO" --pattern 'iina-plugin-danmaku.iinaplgz' --dir IINA+ --clobber
+fi
 
-# Install webview deps
-cd IINA+/WebFiles/
-npm install
+( cd IINA+/WebFiles/ && npm install )
+
+if [ "${CI:-}" = "true" ]; then
+  read -r VERSION BUILD <<< $(unzip -p IINA+/iina-plugin-danmaku.iinaplgz Info.json | python3 -c "import json,sys;d=json.load(sys.stdin);print(d['version'],d['ghVersion'])")
+  sed -i '' "s/static let internalPluginVersion = \"[^\"]*\"/static let internalPluginVersion = \"$VERSION\"/" IINA+/Utils/IINAApp.swift
+  sed -i '' "s/static let internalPluginBuild = [0-9]*/static let internalPluginBuild = $BUILD/" IINA+/Utils/IINAApp.swift
+fi
